f7af91a62ee6f5cf237f6d37e2134a38a5831c1e
Yohann-dev <yohann_88@hotmail.fr>
2025-05-05 18:39:22 +0300

    caching data

--- server/src/controllers/marketController.ts
+++ server/src/controllers/marketController.ts
@@ -1,59 +1,50 @@
 import axios from "axios"
 
+let cachedMarketData: any = null;
+let cacheTimestamp: number = 0;
+const CACHE_DURATION_MS = 60 * 60 * 1000;
+
 export const marketController = {
 
     getBelieveDataFiltered(data: any) {
         const mapData = new Map();
 
-        data.recent.pools.forEach((pool: any) => {
-            mapData.set(pool.id, {
-                price: pool.baseAsset.usdPrice,
-                holderCount: pool.baseAsset.holderCount,
-                marketCap: pool.baseAsset.mcap,
+        const addDataToMap = (data: any) => {
+            data.pools.forEach((pool: any) => {
+                mapData.set(pool.id, {
+                    price: pool.baseAsset.usdPrice?.toFixed(2),
+                    holderCount: pool.baseAsset.holderCount,
+                    marketCap: pool.baseAsset.mcap?.toFixed(0),
+                });
             });
-        });
+        };
 
-        data.graduated.pools.forEach((pool: any) => {
-            mapData.set(pool.id, {
-                price: pool.baseAsset.usdPrice,
-                holderCount: pool.baseAsset.holderCount,
-                marketCap: pool.baseAsset.mcap,
-            });
-        });
-
-        data.aboutToGraduate.pools.forEach((pool: any) => {
-            mapData.set(pool.id, {
-                price: pool.baseAsset.usdPrice?.toFixed(2),
-                holderCount: pool.baseAsset.holderCount,
-                marketCap: pool.baseAsset.mcap?.toFixed(0),
-            });
-        });
+        addDataToMap(data.recent);
+        addDataToMap(data.graduated);
+        addDataToMap(data.aboutToGraduate);
 
         return mapData;
     },
 
     async getBelieveMarketData() {
+        const now = Date.now();
+        if (cachedMarketData && (now - cacheTimestamp < CACHE_DURATION_MS)) {
+            return cachedMarketData;
+        }
+
+        const params = {
+            "timeframe": "24h",
+            "launchpads": [
+                "Believe"
+            ]
+        };
+
         const believeMarketData = await axios.post(
             'https://datapi.jup.ag/v1/pools/gems',
             {
-                "recent": {
-                    "timeframe": "24h",
-                    "launchpads": [
-                        "Believe"
-                    ]
-                },
-                "graduated": {
-                    "timeframe": "24h",
-                    "launchpads": [
-                        "Believe"
-                    ]
-                },
-                "aboutToGraduate": {
-                    "timeframe": "24h",
-                    "launchpads": [
-                        "Believe"
-                    ]
-                }
+                "recent": params,
+                "graduated": params,
+                "aboutToGraduate": params
             }, {
                 headers: {
                     "Accept": "application/json",
@@ -63,6 +54,10 @@ export const marketController = {
         );
 
         const filterData = this.getBelieveDataFiltered(believeMarketData.data);
+
+        // Cache the result and timestamp
+        cachedMarketData = filterData;
+        cacheTimestamp = now;
 
         return filterData;
     }
