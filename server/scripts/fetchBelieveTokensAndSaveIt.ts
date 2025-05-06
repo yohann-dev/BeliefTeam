1cdae21fa1ff35ec8f48677761ad1d2b9db909db
Yohann-dev <yohann_88@hotmail.fr>
2025-05-06 19:58:31 +0300

    improved query script for twitter scrapping

--- server/scripts/fetchBelieveTokensAndSaveIt.ts
+++ server/scripts/fetchBelieveTokensAndSaveIt.ts
@@ -4,7 +4,6 @@ import { db } from '../firebase.ts';
 
 const getTweetRepliesFromlaunchcoins = async () => {
     const scraper = new Scraper();
-    const username = 'launchcoin';
     const tokensData: any[] = [];
     
     await scraper.login(
@@ -18,14 +17,83 @@ const getTweetRepliesFromlaunchcoins = async () => {
         process.env.TWITTER_ACCESS_TOKEN_SECRET
     );
 
-    const tweetsReplies = await scraper.searchTweets(`from:${username}`, 50, SearchMode.Latest);
+    let isLast = false;
+    let nextCursor;
+    const [lastTimestampSaved, lastTweetIdSaved] = await getLastTimestampAndLastTweetIdSaved();
+    let newestLastTimestamp = lastTimestampSaved;
+    let lastTweetIdToSave = lastTweetIdSaved;
 
-    for await (const tweet of tweetsReplies) {
-        const tokenData = getTokenData(tweet);
-        tokensData.push(tokenData);
+    
+    while (!isLast) {
+        console.log(`Fetching tweets from ${nextCursor}`);
+        let response;
+        try {
+            response = await fetchTweetsFromTwitter(scraper, nextCursor);
+
+        } catch (error) {
+            console.error(`Error fetching tweets from ${nextCursor}:`, error);
+            await waitForXSeconds(60);
+            continue;
+        }
+        nextCursor = response.next;
+        if (!nextCursor) {
+            isLast = true;
+        }
+
+        if (response.tweets.length === 0) {
+            isLast = true;
+            continue;
+        }
+
+        for (const tweet of response.tweets) {
+            if (tweet.id === lastTweetIdSaved) {
+                isLast = true;
+                break;
+            }
+
+            const createdAt = tweet.timestamp;
+            if (createdAt && createdAt > newestLastTimestamp) {
+                newestLastTimestamp = createdAt;
+                lastTweetIdToSave = tweet.id || '';
+            }
+
+            const tokenData = getTokenData(tweet);
+            if (!tokenData.tokenAddress || tweet?.text?.includes('Failed to launch your coin.')) continue;
+
+            console.log(`Fetched: Token from @${tokenData.author} - with address:${tokenData.tokenAddress}`);
+            tokensData.push(tokenData);
+        }
+
+        await waitForXSeconds(10);
     }
 
+    await saveLastTimestampAndLastTweetId(newestLastTimestamp, lastTweetIdToSave);
+
     return tokensData;
+};
+
+const getLastTimestampAndLastTweetIdSaved = async (): Promise<[number, string]> => {
+    const metaRef = db.collection("meta").doc("scraper");
+    const metaSnap = await metaRef.get();
+    const lastTimestamp = metaSnap.exists ? metaSnap.data()?.lastTweetTimestamp : 0;
+    const lastTweetId = metaSnap.exists ? metaSnap.data()?.lastSavedTweetId : '';
+
+    return [lastTimestamp, lastTweetId];
+};
+
+const saveLastTimestampAndLastTweetId = async (lastTimestamp: number, lastTweetId: string) => {
+    const metaRef = db.collection("meta").doc("scraper");
+    await metaRef.set({ lastTweetTimestamp: lastTimestamp, lastSavedTweetId: lastTweetId }, { merge: true });
+};
+
+const waitForXSeconds = (seconds: number) => {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+};
+
+const fetchTweetsFromTwitter = async (scraper: Scraper, cursor?: string) => {
+    const tweetsReplies = await scraper.fetchSearchTweets('from:launchcoin', 50, SearchMode.Latest, cursor);
+
+    return tweetsReplies;
 };
 
 const getTokenData = (tweetReply: any) => {
@@ -51,24 +119,35 @@ const getTokenData = (tweetReply: any) => {
     };
 };
 
-const saveTokensToFirebase = async (tokensData: any) => {
-    // Save only if the token is not already in the database
-    const tokensRef = db.collection('tokens');
-    for (const tokenData of tokensData) {
-        const tokenDoc = await tokensRef.where('tokenAddress', '==', tokenData.tokenAddress).get();
-        if (tokenDoc.empty) {
-            await tokensRef.add(tokenData);
-            console.log(`Token with address ${tokenData.tokenAddress} added to the database.`);
-        } else {
-            console.log(`Token with address ${tokenData.tokenAddress} already exists in the database.`);
+const saveTokensInBatchToFirebase = async (tokensData: any) => {
+    console.log(`Saving ${tokensData.length} tokens in the database.`);
+    try {
+        const tokensRef = db.collection('tokens');
+
+    const batchSize = 400;
+    for (let i = 0; i < tokensData.length; i += batchSize) {
+        const batch = db.batch();
+
+        for (const tokenData of tokensData.slice(i, i + batchSize)) {
+            console.log(`Saving token ${tokenData.tokenAddress} in the database.`);
+            const ref = tokensRef.doc(tokenData.tokenAddress);
+            batch.set(ref, tokenData, { merge: true });
         }
+
+            await batch.commit();
+        }
+    } catch (error) {
+        console.error(`Error saving tokens in the database:`, error);
     }
 };
 
 const fetchBelieveTokensAndSaveIt = async () => {
     const tokensDataFromlaunchcoins = await getTweetRepliesFromlaunchcoins();
 
-    // await saveTokensToFirebase(tokensDataFromlaunchcoins);
+    await saveTokensInBatchToFirebase(tokensDataFromlaunchcoins);
+
+    console.log('All tokens saved in the database.');
+    process.exit(0);
 };
 
 fetchBelieveTokensAndSaveIt();
