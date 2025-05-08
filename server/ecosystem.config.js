a35831c7ec0acf4c2275884ef222fecc312172b5
Yohann-dev <yohann_88@hotmail.fr>
2025-05-08 15:43:30 +0300

    added pm2

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: 'beliefteam-server',
+    script: 'dist/server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
+}; 
