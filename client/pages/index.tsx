81760ea4f703d493a3c9bc0a8a55bce0cad6e138
Yohann-dev <yohann_88@hotmail.fr>
2025-05-08 23:20:23 +0300

    changed title color

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -39,8 +39,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
           <div className="text-center">
             <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block">Belief</span>
-              <span className="block text-meme-blue">Team</span>
+              <span className="bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent">BeliefTeam</span>
             </h1>
             
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-20 md:text-xl md:max-w-3xl animate-bounce">
